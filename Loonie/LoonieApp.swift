20f5e6896bcd47f2b501eda93009b9f42b8ed76a
KyleClight <belkius102@gmail.com>
2026-03-15 23:29:55 +0600

    Initial Commit

---
+++ Loonie/LoonieApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoonieApp.swift
+//  Loonie
+//
+//  Created by ll on 15.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct LoonieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
