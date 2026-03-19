ac91115da658c6a22f3916db33eb8297a7f4cca0
KyleClight <belkius102@gmail.com>
2026-03-19 18:26:00 +0600

    add: base stack for the practice

---
+++ Loonie/LoginView.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct LoginView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var isLoggedIn: Bool = false
+    @State private var iAdmin: Bool = false
+    
+    var body: some View {
+        ZStack(alignment: .center) {
+            
+            VStack() {
+                
+                HStack() {
+                    
+                }
+            }
+        }
+    }
+}
+
+
--- Loonie/LoonieApp.swift
+++ Loonie/LoonieApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LoonieApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
